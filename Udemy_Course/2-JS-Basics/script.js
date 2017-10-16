8202384945f3a1764eb21695cae1e4f7c1508c73
Unknown <sebastiaan.westdijk@eneco.com>
2017-10-16 16:27:28 +0200

    Section 2: college 10 done

--- Udemy_Course/2-JS-Basics/script.js
+++ Udemy_Course/2-JS-Basics/script.js
@@ -1,1 +1,41 @@
-console.log('Hello World');
+// Lecture: Variables
+/*
+var name = 'John';
+console.log(name);
+
+var lastName = 'Smith';
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+// Lecture: Variables 2
+
+var name = 'John';
+var age = 26;
+
+//console.log(name + age);
+//console.log(age + age)
+
+var job, isMarried;
+
+//console.log(job);
+
+job = 'Teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is the last name?');
+console.log(lastName);
+
+alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
