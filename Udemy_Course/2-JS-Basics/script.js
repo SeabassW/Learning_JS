a35421f847d83b5a91b8405bd9b7da65415729ed
Sebastiaan Westdijk <sebastiaan.westdijk@eneco.com>
2017-10-20 08:53:55 +0200

    Onto first coding challenge

--- Udemy_Course/2-JS-Basics/script.js
+++ Udemy_Course/2-JS-Basics/script.js
@@ -1,3 +1,5 @@
+
+//////////////////////////////////////////////////////////
 // Lecture: Variables
 /*
 var name = 'John';
@@ -13,8 +15,9 @@ var fullAge = true;
 console.log(fullAge);
 */
 
+//////////////////////////////////////////////////////////
 // Lecture: Variables 2
-
+/*
 var name = 'John';
 var age = 26;
 
@@ -39,3 +42,114 @@ var lastName = prompt('What is the last name?');
 console.log(lastName);
 
 alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+
+//////////////////////////////////////////////////////////
+// Lecture: Operators
+
+var now = 2016;
+var birthYear = now - 26;
+
+birthYear = now - 26 * 2;
+
+console.log(birthYear);
+
+var ageJohn = 30;
+var ageMark = 30;
+
+ageJohn = ageMark = (3 + 5) * 4 - 6;
+ageJohn++;
+ageMark *= 2;
+
+console.log(ageJohn);
+console.log(ageMark);
+*/
+//////////////////////////////////////////////////////////
+// Lecture: if/else statements
+/*
+var name = 'John';
+var age = 26;
+var isMarried = 'no'
+
+if (isMarried === 'yes') {
+    console.log(name + ' is married!');
+} else {
+    console.log(name + ' will hopefully marry soon :)');
+}
+
+isMarried = false;
+
+if (isMarried) {
+    console.log('YES!');
+} else {
+    console.log('NO!');
+}
+*/
+//////////////////////////////////////////////////////////
+// Lecture: Boolean logic and Switch
+
+var age = 26;
+
+if (age <= 19) {
+    console.log("John is a teenager");
+} else if (age >= 20 && age < 30) {
+    console.log("John is a young man");
+} else {
+    console.log("John is a man");
+}
+
+
+var job = prompt('What does John do?');
+
+switch (job) {
+    case 'teacher':
+        console.log('John teaches kids');
+        break;
+    case 'driver':
+        console.log('John drives a cab');
+        break;
+    case 'cop':
+        console.log('John fights crime');
+        break;
+    default:
+        console.log('John does nothing');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
