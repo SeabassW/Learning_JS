f651021e994e3970b84fac1ab110e0781d49b54e
Unknown <sebastiaan.westdijk@eneco.com>
2017-10-27 13:49:56 +0200

    Coding Challenge 2 solved

--- Udemy_Course/2-JS-Basics/script.js
+++ Udemy_Course/2-JS-Basics/script.js
@@ -313,7 +313,7 @@ console.log(mike);
 
 /////////////////////////////////////////////////////////////
 // Lecture: Loops
-
+/*
 // for-loops
 for (var i = 0; i < 10; i++) {
     console.log(i);
@@ -346,11 +346,67 @@ for (var i = 1; i <= 5; i++) {
     console.log(i);
 }
 
+*/
+/////////////////////////////////////////////////////////////
+// Coding Challenge 2
 
+/*
+1. Create an array with some years where persons were born
+2. Create an empty array (just [] )
+3. Use a loop to fill the array with the ages of the persons
+4. Use another loop to log into the console whether each person is of full age (18 or older), as well as their age
 
+5. Finally, create a function called printFullAge which receives the array of years as an argument, executes the steps 2., 3. and 4. and returns an array of true/false boolean values: true if the person is of full age (>= 18 years) and false if not (< 18 years)
+6. Call the function with two different arrays and store the results in two variables: full_1 and full_2
 
+Example input:  [1965, 2008, 1992]
+Example output: [true, false, true]
 
+Hint: you can use a loop not only to read from an array, like y[i], but also to set values in an array, like y[i] = ... You can also use the specific array methods.
+*/
+/*
+var yearOfBirth = [1959,1902,2001,1999,2003,2001,2012,1944];
+var age = [];
 
+for (var i = 0; i < yearOfBirth.length; i++) {
+    var currYear = (new Date()).getFullYear();
+    age.push(currYear - yearOfBirth[i]);
+}
+
+console.log(age);
+
+for (var j = 0; j < age.length; j++) {
+    console.log("This person is: "+ age[j] + " years old");
+    if (age[j] >= 18) {
+            console.log("Old Enough");
+    } else {
+        console.log("Too Young!");
+    }
+}
+*/
+var yearOfBirth = [1959,1902,2001,1999,2003,2001,2012,2009];
+
+function printFullAge(arrayYears) {
+    var oldEnough = [];
+    for (var i = 0; i < arrayYears.length; i++) {
+        var currYear = (new Date()).getFullYear();
+        
+        var age = currYear - arrayYears[i];
+            
+        if (age >= 18) {
+            oldEnough.push(true);
+        } else {
+            oldEnough.push(false);
+        }
+    }
+    return oldEnough
+}
+    
+var full_1 = [1965,2008,1992];
+var full_2 = [1998,2002,2016,1999,1959,2000,1823];
+
+console.log(printFullAge(full_1));
+console.log(printFullAge(full_2));
 
 
 
