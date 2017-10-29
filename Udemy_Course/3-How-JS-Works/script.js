ab6179f33f4a76446c092d420d72596184e0a2a5
Unknown <sebastiaan.westdijk@eneco.com>
2017-10-29 14:49:47 +0100

    Section 3 done

--- Udemy_Course/3-How-JS-Works/script.js
+++ Udemy_Course/3-How-JS-Works/script.js
@@ -1,22 +1,37 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+//Function is already defined before execution, so declaration can by before function statement
+/*
+calculateAge(1965);
 
+function calculateAge(year) {
+    console.log(2016 - year);
+}
 
+//Can not be done when using function expressions.
 
+var retirement = function(year) {
+    console.log(65 - (2016 - year));
+}
 
+retirement(1990);
 
+// variables
 
+console.log(age);
+var age = 23;
+console.log(age);
 
+function foo() {
+    var age = 65;
+    console.log(age);
+}
 
+foo();
+console.log(age);
 
-
-
-
-
-
-
-
+*/
 ///////////////////////////////////////
 // Lecture: Scoping
 
@@ -58,14 +73,101 @@ function first() {
 
 function third() {
     var d = 'John';
-    console.log(a + b + c + d);
+    console.log(a + d);
 }
 */
 
-
-
 ///////////////////////////////////////
 // Lecture: The this keyword
+
+// console.log(this);
+
+/*
+calculateAge(1990);
+
+function calculateAge(year) {
+    console.log(2016 - year);
+    console.log(this);
+}
+*/
+
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2016 - this.yearOfBirth); 
+        
+        /*
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+        */
+    }
+};
+
+john.calculateAge();
+
+// Method Borrowing
+
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+};
+
+mike.calculateAge = john.calculateAge;
+
+mike.calculateAge();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
