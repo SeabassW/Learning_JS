e2ca44759a471c18050dd5a5bb1b629e1a05acb4
Unknown <sebastiaan.westdijk@eneco.com>
2017-12-13 15:08:45 +0100

    Made awesome video player skin

---
+++ JavaScript30/11 - Custom Video Player/scripts.js
@@ -0,0 +1,83 @@
+// Add fullscreen to video player
+
+/* Get our elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+const buttonFS = player.querySelector('.fullscreen');
+
+
+/* Build our functions */
+
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+// Listen for the event of video pausing / playing
+function updateButton() {
+    const icon = this.paused ? '►' : '▐ ▌';
+    toggle.textContent = icon;
+}
+
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    console.log(this.value);
+    console.log(this.name);
+
+    video[this.name] = this.value;
+}
+
+// Listen for time to update on the video
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
+function toggleFullscreen() {
+    if (!document.webkitFullscreenElement) {
+        player.webkitRequestFullScreen();
+    } else {
+        document.webkitCancelFullScreen();
+    }
+}
+
+/* Hook up the event listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+
+let mouseDown = false;
+progress.addEventListener('click', scrub);
+
+// Only moves on to scrub fn if mouseDown = true
+progress.addEventListener('mousemove', (e) => mouseDown && scrub(e));
+progress.addEventListener('mousedown', () => mouseDown = true);
+progress.addEventListener('mouseup', () => mouseDown = false);
+
+buttonFS.addEventListener('click', toggleFullscreen);
+
+
+
