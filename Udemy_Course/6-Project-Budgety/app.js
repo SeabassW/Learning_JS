84216ca5242cb7c94657f62fb39d4e44e3cec5f2
SEABASS-PC\Seabass <sebastiaan.westdijk@eneco.com>
2018-03-08 13:30:24 +0100

    Done 73

--- Udemy_Course/6-Project-Budgety/app.js
+++ Udemy_Course/6-Project-Budgety/app.js
@@ -27,6 +27,37 @@ var budgetController = (function() {
         }
     };
 
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            //[1 2 3 4 5], next ID = 6
+            //[1 2 4 6 8], next ID = 9
+            //ID = last ID + 1
+            //Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            //Create new item based on type
+            if (type = 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type = 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // Push it into data structure
+            data.allItems[type].push(newItem);
+
+            // Return it
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+    };
 })();
 
 
@@ -80,11 +111,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 
     var ctrlAddItem = function() {
+        var input, newItem;
 
         //1. Get the field input date
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
 
         //2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         //3. Add the item to the the UI
 
